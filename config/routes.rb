5cfb2d901ef21239682798dbc1929403d764e386
Flavio <flavio.cardemil@gmail.com>
2021-07-22 22:50:36 -0400

    normal

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'projects/dashboard'
   post 'projects/create'
   get 'projects/new'
-  root 'projects#dashboard'
+  root 'projects#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
